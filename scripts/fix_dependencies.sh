4ee644dbd216b4de0caf452e5f24d0959fe5b843
Ming Wang <oumei.vip@gmail.com>
2014-03-21 13:36:54 +0800

    add dependency of bson_ext

--- scripts/fix_dependencies.sh
+++ scripts/fix_dependencies.sh
@@ -5,4 +5,4 @@ gem install logging nokogiri rubyntlm httpclient
 echo 'installing viewpoint-1.0.0...'
 gem install viewpoint-1.0.0.beta.2.gem --local
 echo 'installig mongodb driver for ruby'
-gem install mongo
+gem install mongo bson_ext
