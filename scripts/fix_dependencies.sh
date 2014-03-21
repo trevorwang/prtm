a5c5a3d23590460b18a27aec9ea44a5f4a1c128f
Ming Wang <oumei.vip@gmail.com>
2014-03-21 13:32:53 +0800

    add scripts to fix dependencies for this project

---
+++ scripts/fix_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo 'installing viewpoint dependencies'
+gem install logging nokogiri rubyntlm httpclient
+echo 'installing viewpoint-1.0.0...'
+gem install viewpoint-1.0.0.beta.2.gem --local
+echo 'installig mongodb driver for ruby'
+gem install mongo
